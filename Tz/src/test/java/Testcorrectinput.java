d7943069e3ed9d1be532961b273126da5c9c7054
smallbebrych <167618145+smallbebrych@users.noreply.github.com>
2024-06-09 15:36:10 +0300

    Update Testcorrectinput.java

--- Tz/src/test/java/Testcorrectinput.java
+++ Tz/src/test/java/Testcorrectinput.java
@@ -14,13 +14,4 @@ public class Testcorrectinput {
         boolean hasLetter = Arrays.stream(numbersString).anyMatch(s -> s.matches(".*[a-zA-Z].*"));
         assertEquals(hasLetter, false);
         }
-    @Test
-    public void test2() throws FileNotFoundException {
-        File file = new File("C:\\Users\\Глеб\\IdeaProjects\\Tz\\src\\test\\Tests\\wrongtestforcorrectinput.txt");
-        Scanner scanner = new Scanner(file);
-        String line = scanner.nextLine();
-        String[] numbersString = line.split(" ");
-        boolean hasLetter = Arrays.stream(numbersString).anyMatch(s -> s.matches(".*[a-zA-Z].*"));
-        assertEquals(hasLetter, false);
-    }
 }
